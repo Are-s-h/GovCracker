c9200cd1d706aa12ed5bc157793bfbfd2dd40ec5
Are§h <73139495+Are-s-h@users.noreply.github.com>
2022-12-02 12:58:56 +0100

    Update

--- src/About.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.ComponentModel
-
-Public Class About
-    Private Sub About_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
-        GovTools.BringToFront()
-    End Sub
-End Class
--- src/MaskHelp.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.ComponentModel
-
-Public Class MaskHelp
-    Private Sub MaskHelp_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
-        GovTools.BringToFront()
-    End Sub
-End Class
